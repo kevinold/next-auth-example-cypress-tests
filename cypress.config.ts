647c2dea8936ee4d87f790ee51e9a3848ac8c342
Kevin Old <kevin@kevinold.com>
2023-09-12 10:55:00 -0500

    import .env.local for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+import { encode } from "next-auth/jwt";
+
+dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        async encodeJwt(token) {
+          const encToken = await encode({
+            token,
+            secret: process.env.NEXTAUTH_SECRET,
+          });
+          return encToken;
+        },
+      });
     },
   },
 });
